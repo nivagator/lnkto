498f6675c63efe21dfcb563b9720dd296f779bd6
Gavin Greer <gavingreer@mac.com>
2017-10-24 00:12:05 -0500

    basic shortener added. end of day 1

--- shortener/models.py
+++ shortener/models.py
@@ -1,11 +1,26 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
 
-class LnktoURL(models.Model):
-    url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15, unique=True)
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
+
+class LnktoURL(models.Model):
+    url         = models.CharField(max_length=220, )
+    shortcode   = models.CharField(max_length=15, unique=True)
+    updated     = models.DateTimeField(auto_now=True) #everytime the model is saved
+    timestampo  = models.DateTimeField(auto_now_add=True) #when model is created
+    # empty_datetime = models.DateTimeField(auto)
+
+    def save(self, *args, **kwargs):
+        print("something")
+        self.shortcode = code_generator()
+        super(LnktoURL, self).save(*args, **kwargs)
+
+    
 
     def __str__(self):
         return str(self.url)
